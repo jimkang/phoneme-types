55b0cf7571d755bc63ac14ae320dbc61b4a84d92
Jim Kang <jimkang@gmail.com>
2015-02-08 22:30:25 -0500

    Subsumed phoneme-navigator from homophonizer project for additional phoneme classication methods.

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
 test:
-	node tests/basictests.js
+	node tests/consonant-vowel-tests.js
+	node_modules/mocha/bin/mocha --ui tdd -R spec tests/basictests.js
+
--- phoneme-types.js
+++ phoneme-types.js
@@ -1,56 +1,136 @@
-var vowelishPhonemes = [
-  'AA',
-  'AE',
-  'AH',
-  'AO',
-  'AW',
-  'AY',
-  'EH',
-  'ER',
-  'EY',
-  'IH',
-  'IY',
-  'OW',
-  'OY',
-  'UH',
-  'UW'
-];
+// This is a singleton-style module.
+var _ = require('lodash');
 
-var consonantishPhonemes = [
-  'B',
-  'CH',
-  'D',
-  'DH',
-  'F',
-  'G',
-  'HH',
-  'JH',
-  'K',
-  'L',
-  'M',
-  'N',
-  'NG',
-  'P',
-  'R',
-  'S',
-  'SH',
-  'T',
-  'TH',
-  'V',
-  'W',
-  'Y',
-  'Z',
-  'ZH'
-];
-
-module.exports = {
-  vowelish: vowelishPhonemes,
-  consonantish: consonantishPhonemes,
-  isConsonantish: function isConsonantish(phoneme) {
-    return (-1 !== consonantishPhonemes.indexOf(phoneme));
-  },
-  isVowelish: function isVowelish(phoneme) {
-    return (-1 !== vowelishPhonemes.indexOf(phoneme));
-  }
+var phonemeListsByClassification = {
+  vowel: [
+    'AA',
+    'AE',
+    'AH',
+    'AO',
+    'AW',
+    'AY',
+    'EH',
+    'ER',
+    'EY',
+    'IH',
+    'IY',
+    'OW',
+    'OY',
+    'UH',
+    'UW'
+  ],
+  affricate: [
+    'CH',
+    'JH'
+  ],
+  aspirate: [
+    'HH'
+  ],
+  fricative: [
+    'DH',
+    'F',
+    'S',
+    'SH',
+    'TH',
+    'V',
+    'Z',
+    'ZH'
+  ],
+  liquid: [
+    'L',
+    'R'
+  ],
+  nasal: [
+    'M',
+    'N',
+    'NG'
+  ],
+  semivowel: [
+    'W',
+    'Y'
+  ],
+  stop: [
+    'B',
+    'D',
+    'G',
+    'K',
+    'P',
+    'T'
+  ]
 };
 
+var classificationsByPhoneme = {
+  AA: 'vowel',
+  AE: 'vowel',
+  AH: 'vowel',
+  AO: 'vowel',
+  AW: 'vowel',
+  AY: 'vowel',
+  EH: 'vowel',
+  ER: 'vowel',
+  EY: 'vowel',
+  IH: 'vowel',
+  IY: 'vowel',
+  OW: 'vowel',
+  OY: 'vowel',
+  UH: 'vowel',
+  UW: 'vowel',
+  CH: 'affricate',
+  JH:'affricate',
+  HH: 'aspirate',
+  DH: 'fricative',
+  F: 'fricative',
+  S: 'fricative',
+  SH: 'fricative',
+  TH: 'fricative',
+  V: 'fricative',
+  Z: 'fricative',
+  ZH: 'fricative',
+  L: 'liquid',
+  R: 'liquid',
+  M: 'nasal',
+  N: 'nasal',
+  NG: 'nasal',
+  W: 'semivowel',
+  Y: 'semivowel',
+  B: 'stop',
+  D: 'stop',
+  G: 'stop',
+  K: 'stop',
+  P: 'stop',
+  T: 'stop'
+};
+
+function classifyPhoneme(phoneme) {
+  return (phoneme in classificationsByPhoneme) ? 
+    classificationsByPhoneme[phoneme] : null;
+}
+
+function getPhonemesInSameClass(phoneme) {
+  var fellows = [];
+  var phonemeClass = classifyPhoneme(phoneme);
+  if (phonemeClass) {
+    fellows = _.without(phonemeListsByClassification[phonemeClass], phoneme);
+  }
+  return fellows;
+}
+
+function stripStressor(phonemeWithStressor) {
+  return phonemeWithStressor.replace(/\d/g, '');
+}
+
+function isConsonantish(phoneme) {
+  return classificationsByPhoneme[phoneme] !== 'vowel';
+}
+
+function isVowelish(phoneme) {
+  return classificationsByPhoneme[phoneme] === 'vowel';
+}
+
+module.exports = {
+  classifyPhoneme: classifyPhoneme,
+  stripStressor: stripStressor,
+  getPhonemesInSameClass: getPhonemesInSameClass,
+  isConsonantish: isConsonantish,
+  isVowelish: isVowelish
+};
